de1014492975ed555448c53f3f483694a83be4d7
atleSkaanes <atle.skaanes@gmail.com>
2026-07-10 17:11:32 +0200

    feat(programs/noctalia): Added config

--- home-manager/programs/noctalia.nix
+++ home-manager/programs/noctalia.nix
@@ -24,61 +24,97 @@
   # configure options
   programs.noctalia = {
     enable = true;
-    # package = (pkgs.noctalia-shell.override { calendarSupport = true; });
-    # settings = {
-    #   # configure noctalia here; defaults will
-    #   # be deep merged with these attributes.
-    #   bar = {
-    #     density = "compact";
-    #     position = "top";
-    #     showCapsule = false;
-    #     widgets = {
-    #       left = [
-    #         {
-    #           id = "SidePanelToggle";
-    #           useDistroLogo = true;
-    #         }
-    #         {
-    #           id = "WiFi";
-    #         }
-    #         {
-    #           id = "Bluetooth";
-    #         }
-    #       ];
-    #       center = [
-    #         {
-    #           hideUnoccupied = false;
-    #           id = "Workspace";
-    #           labelMode = "none";
-    #         }
-    #       ];
-    #       right = [
-    #         {
-    #           alwaysShowPercentage = false;
-    #           id = "Battery";
-    #           warningThreshold = 30;
-    #         }
-    #         {
-    #           formatHorizontal = "HH:mm:ss";
-    #           formatVertical = "HH mm";
-    #           id = "Clock";
-    #           useMonospacedFont = true;
-    #           usePrimaryColor = true;
-    #         }
-    #       ];
-    #     };
-    #   };
-    #   colorSchemes.predefinedScheme = "Monochrome";
-    #   general = {
-    #     avatarImage = "/home/drfoobar/.face";
-    #     radiusRatio = 0.2;
-    #   };
-    #   location = {
-    #     monthBeforeDay = true;
-    #     name = "Marseille, France";
-    #   };
-    # };
-    # # this may also be a string or a path to a JSON file,
-    # # but in this case must include *all* settings.
+    settings = {
+      bar.default = {
+        background_opacity = 0.0;
+        capsule = true;
+        capsule_opacity = 0.95;
+        margin_edge = 5;
+        margin_end = 10;
+        scale = 0.95;
+        thickness = 30;
+
+        start = [
+          "control-center"
+          "session"
+          "media"
+          "group:stats"
+        ];
+        center = [ "workspaces" ];
+        end = [
+          "privacy"
+          "tray"
+          "clipboard"
+          "group:connections"
+          "group:sliders"
+          "battery"
+          "clock"
+          "notifications"
+        ];
+
+        capsule_group = [
+          {
+            fill = "surface_variant";
+            id = "connections";
+            members = [ "network" "bluetooth" ];
+          }
+          {
+            fill = "surface_variant";
+            id = "sliders";
+            members = [ "volume" "brightness" ];
+          }
+          {
+            fill = "surface_variant";
+            id = "stats";
+            members = [ "cpu" "ram" "temp" ];
+          }
+        ];
+      };
+
+      lockscreen = {
+        blurred_desktop = true;
+      };
+
+      osd = {
+        position = "top_right";
+        position_vertical = "bottom_right";
+        kinds = {
+          media = false;
+        };
+      };
+
+      shell = {
+        time_format = "{:%H:%M:%S}";
+        panel = {
+          control_center_placement = "floating";
+          open_near_clicl_control_center = true;
+        };
+      };
+
+      theme = {
+        builtin = "Catppuccin";
+        community_palette = "Catppuccin Lavender";
+        mode = "dark";
+        source = "builtin";
+      };
+
+      wallpaper.enable = false;
+
+      widget = {
+        clock.format = "{:%H:%M:%S}";
+
+        cpu.show_label = false;
+        ram.show_label = false;
+        temp.show_label = false;
+
+        tray.drawer = true;
+
+        workspaces = {
+          empty_color = "surface_variant";
+          minimal = true;
+          occupied_color = "on_surface_variant";
+        };
+      };
+    };
   };
 }
