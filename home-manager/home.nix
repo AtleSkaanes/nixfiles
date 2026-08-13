68d067641efc07cab2be771d8e1ed9271649d7bb
Atle Skaanes Ahrenfeldt <atle.skaanes@gmail.com>
2026-08-13 21:01:45 +0200

    fix: Enabled pointerCursor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -13,6 +13,8 @@
   home.username = "atle";
   home.homeDirectory = "/home/atle";
 
+  home.pointerCursor.enable = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
